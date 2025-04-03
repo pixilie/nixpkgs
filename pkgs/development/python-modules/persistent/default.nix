f9704f1fe5395c1f5d92b8fa132da5c1dee50797
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-04-03 04:18:19 +0200

    fixup! python3Packages.persistent: 6.1 -> 6.1.1

--- pkgs/development/python-modules/persistent/default.nix
+++ pkgs/development/python-modules/persistent/default.nix
@@ -28,7 +28,7 @@ buildPythonPackage rec {
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace-fail "setuptools<74" "setuptools"
+      --replace-fail "setuptools < 74" "setuptools"
   '';
 
   build-system = [ setuptools ];
